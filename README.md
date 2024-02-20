548004a1ecd30073a57e72d1770dd9f5dbd2a2df
Emmanuel Tiboah <etiboah@gmail.com>
2024-02-20 14:56:41 +0000

    update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Student CRUD REST API
+
+This is a simple crud rest api to go through the [SRE Bootcamp exercise](https://playbook.one2n.in/sre-bootcamp/sre-bootcamp-exercises)
+
+# Architecture Diagram
+![Student Crud API architecture diagram](docs/student_crud_api.png)
--- src/routes/student.py
+++ src/routes/student.py
@@ -31,12 +31,12 @@ def create_student(student: StudentInput, db: Session= Depends(get_db)):
     return new_student
 
 @router.patch("/{student_id}", status_code=status.HTTP_200_OK, response_model=StudentResponse)
-def update_student(student_id: str, student: StudentInput, db: Session=Depends(get_db)):
+def update_student(student_id: str, student_input: StudentInput, db: Session=Depends(get_db)):
     student = db.query(Student).filter(Student.id == student_id)
     student_in_db = student.first()
     if not student_in_db:
         raise NotFoundError(status_code=status.HTTP_404_NOT_FOUND, error_msg=f"Student with id {student_id} not found")
-    updated_dict = student.dict()
+    updated_dict = student_input.dict()
     new_update = {k: v for k, v in updated_dict.items() if v is not None}
 
     student.update(new_update)
